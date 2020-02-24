ae0decf7fc0bc80e94da1af63616338e7c77cd8c
Carlton Lindsay <carlton.l@outlook.com>
2020-02-23 18:23:13 -0800

    Reverted to JQuery scrolling

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 // JQuery Smooth Scroll
-// $('.nav-bar a').on('click', function(e) {
-//    if (this.hash !== '') {
-//        e.preventDefault();
+$('.nav-bar a').on('click', function(e) {
+   if (this.hash !== '') {
+       e.preventDefault();
 
-//        const hash = this.hash;
+       const hash = this.hash;
 
-//        $('html, body').animate({
-//            scrollTop: $(hash).offset().top
-//         }, 800);
-//    }
-// });
+       $('html, body').animate({
+           scrollTop: $(hash).offset().top
+        }, 800);
+   }
+});
 
 // SmoothScroll script
-var scroll = new SmoothScroll('.nav-bar a', {
-    speed: 600
-});
+// var scroll = new SmoothScroll('.nav-bar a', {
+//     speed: 600
+// });
