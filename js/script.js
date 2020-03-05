c4860c51873f4077f964f27725ecbf2e644d8f19
Carlton Lindsay <carlton.l@outlook.com>
2020-03-04 21:08:39 -0800

    Moved js to separate file
    + updated easing speed

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function openModal() {
+  document.getElementById("aboutPage").style.width = "100%";
+  document.querySelector(".modal--content").style.opacity = "100%"
+}
+
+function closeModal() {
+  document.getElementById("aboutPage").style.width = "0%";
+  document.querySelector(".modal--content").style.opacity = "0%"
+}
