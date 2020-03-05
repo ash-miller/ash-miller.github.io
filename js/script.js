e98f0faea9401b3fdf0278d9d97173fc3270c9ac
Carlton Lindsay <carlton.l@outlook.com>
2020-03-04 22:47:40 -0800

    Added responsive features to modal

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@ function closeModal() {
   document.getElementById("aboutPage").style.width = "0%";
   document.querySelector(".modal--content").style.opacity = "0%"
 }
+
+const copyToClipboard = str => {
+  const el = document.createElement('textarea');
+  el.value = str;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+};
+
+var init = (function(){
+  document.querySelector('.social-link--email').addEventListener('click', copyToClipboard('miashlei@amazon.com'));
+})();
